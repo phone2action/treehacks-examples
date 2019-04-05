bf6fb64cf0561e946b9c2ebd04757247460396f8
liamrathke <liam.rathke@gmail.com>
2019-04-05 18:09:01 -0400

    Fixes up VueJS app
    Title

---
+++ node/examples/find-legislators-web/src/components/ListElement.vue
@@ -0,0 +1,25 @@
+<template>
+  <li class="list-group-item">
+    <div class="row text-left">
+      <div class="col-2 text-center">
+        <b>{{title}}</b>
+      </div>
+      <div class="col-10">
+        <slot></slot>
+      </div>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'ListElement',
+  props: {
+    title: {
+      type: String, 
+      required: true
+    }
+  }
+}
+</script>
+
